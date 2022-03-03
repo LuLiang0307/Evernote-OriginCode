379ba4e638870d601eac16bea58b77f6935d6d3b
Lucia <2313970630@qq.com>
2022-03-03 14:23:55 +0800

    feat<通用函数>：封装friendlyDate函数，格式化时间

---
+++ src/helpers/utils.js
@@ -0,0 +1,22 @@
+export function friendlyDate(datesStr) {
+    let dateObj = typeof datesStr === 'object' ? datesStr : new Date(datesStr)
+    let time = dateObj.getTime()
+    let now = new Date()
+    let space = now - time
+    let str = ''
+    switch (true) {
+        case space < 1000 * 60:
+            str = '刚刚'
+            break
+        case space < 1000 * 60 * 60:
+            str = Math.floor(space / (1000 * 60)) + '分钟前'
+            break
+        case space < 1000 * 60 * 60 * 24:
+            str = Math.floor(space / (1000 * 60 * 60)) + '小时前'
+            break
+        default:
+            str = Math.floor(space / (1000 * 60 * 60 * 24)) + '天前'
+            break
+    }
+    return str
+}
